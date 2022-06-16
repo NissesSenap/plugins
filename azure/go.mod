7a77fb005a3f2941b8099230d16bacf693afa4d9
edvin norling <edvin.norling@gmail.com>
2022-06-16 08:31:13 +0200

    Auth azure

---
+++ azure/go.mod
@@ -0,0 +1,10 @@
+module github.com/falcosecurity/plugins/azure/k8saudit
+
+go 1.15
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.1.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/operationalinsights/armoperationalinsights v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.0.0
+)
---
+++ azure/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/operationalinsights/armoperationalinsights"
+)
+
+var (
+	subscriptionID    string
+	resourceGroupName string
+	workspaceName     string
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+	resourceGroupName = os.Getenv("AZURE_RESOURCE_GROUP")
+	workspaceName = os.Getenv("AZURE_WORKSPACE_NAME")
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	workspace, err := getWorkspace(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("operational insights get workspace:", *workspace.ID)
+
+	workspaces, err := listWorkspace(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, w := range workspaces {
+		log.Println(*w.Name, *w.ID)
+	}
+}
+
+func getWorkspace(ctx context.Context, cred azcore.TokenCredential) (*armoperationalinsights.Workspace, error) {
+	workspacesClient, err := armoperationalinsights.NewWorkspacesClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := workspacesClient.Get(ctx, resourceGroupName, workspaceName, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Workspace, nil
+}
+
+func listWorkspace(ctx context.Context, cred azcore.TokenCredential) ([]*armoperationalinsights.Workspace, error) {
+	workspacesClient, err := armoperationalinsights.NewWorkspacesClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	workspaceResp := workspacesClient.NewListByResourceGroupPager(resourceGroupName, nil)
+	pager, err := workspaceResp.NextPage(ctx)
+	if err != nil {
+		return nil, err
+	}
+	workspaces := make([]*armoperationalinsights.Workspace, 0)
+	workspaces = append(workspaces, pager.Value...)
+	return workspaces, nil
+}
